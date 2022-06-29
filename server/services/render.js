3cfd3a3b77292c6090f486b3930e600478aafed5
Antony Anil <anil.antony@settlin.in>
2022-06-29 18:42:10 +0530

    Rest API added

--- server/services/render.js
+++ server/services/render.js
@@ -2,8 +2,12 @@ const axios = require('axios');
 
 //Get request for Homepage
 exports.homeroute = (req,res) => {
-    res.render('index',{
-        nav : [{'links':'/author','title':'Auhtor'},{'links':'/books','title':'Books'},{'links':'/AddBook','title':'AddBook'}],
-        title : 'Library'
-    })
+    axios.get('http://localhost:5000/api/books')
+        .then(response=>{
+            res.render('index',{
+                nav : [{'links':'/author','title':'Auhtor'},{'links':'/books','title':'Books'},{'links':'/AddBook','title':'AddBook'}],
+                title : 'Library',books : response.data 
+            })
+        })
+    
 }
