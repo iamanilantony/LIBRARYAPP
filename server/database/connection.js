ab98adb03f12f2230748f67b3b49ca7a99dc0ee3
Anil Antony <92921196+AnilAntony3134@users.noreply.github.com>
2022-07-11 08:00:33 +0530

    Session Coonection

--- server/database/connection.js
+++ server/database/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-const uri = 'mongodb://localhost:27017/library'
+// const uri = 'mongodb://localhost:27017/library'
 
 const connectDB = async() => {
     try {
--- server/database/sessionconnection.js
+++ server/database/sessionconnection.js
@@ -1,9 +1,9 @@
 const sessions = require('express-session')
 const MongoDBSession = require('connect-mongodb-session')(sessions)
-const uri = 'mongodb://localhost:27017/library'
+// const uri = 'mongodb://localhost:27017/library'
 
 const sessionstore = new MongoDBSession({
-    uri: uri,
+    uri: process.env.MONGO_URI,
     collection: 'sessions'
 })
 
